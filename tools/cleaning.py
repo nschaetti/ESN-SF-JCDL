dc2bc5f362b4a7722a56a26c725e265e38ed9e7c
nschaetti <n.schaetti@gmail.com>
2017-12-01 12:08:21 +0100

    Remove _

---
+++ tools/cleaning.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+# Remove _
+def remove_underline(text):
+    """
+    Remove _
+    :param text:
+    :return:
+    """
+    return text.replace(u"_", u"")
+# end remove_underline
