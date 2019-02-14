0859e87b6a4413c04f35da891678f0779ae8c6e8
nschaetti <n.schaetti@gmail.com>
2019-02-14 10:16:17 +0100

    Functions

--- tools/functions.py
+++ tools/functions.py
@@ -3,10 +3,80 @@
 #
 
 # Imports
-import nsNLP
 import sys
 import pickle
 import os
+import echotorch.nn as etnn
+
+
+# Manage W
+def manage_w(xp, args, keep_w):
+    """
+    Manage W
+    :param xp:
+    :param args:
+    :param keep_w:
+    :return:
+    """
+    # First params
+    rc_size = int(args.get_space()['reservoir_size'][0])
+    rc_w_sparsity = args.get_space()['w_sparsity'][0]
+
+    # Create W matrix
+    w = etnn.ESNCell.generate_w(rc_size, rc_w_sparsity)
+
+    # Save classifier
+    if keep_w:
+        xp.save_object(u"w", w)
+    # end if
+
+    return w
+# end manage_w
+
+
+# Get params
+def get_params(space):
+    """
+    Get params
+    :param space:
+    :return:
+    """
+    # Params
+    reservoir_size = int(space['reservoir_size'])
+    w_sparsity = space['w_sparsity']
+    leak_rate = space['leak_rate']
+    input_scaling = space['input_scaling']
+    input_sparsity = space['input_sparsity']
+    spectral_radius = space['spectral_radius']
+    ridge_param = space['ridge_param']
+    feature = space['transformer'][0][0]
+    aggregation = space['aggregation'][0][0]
+    state_gram = space['state_gram']
+    feedbacks_sparsity = space['feedbacks_sparsity']
+    lang = space['lang'][0][0]
+    embedding = space['embedding'][0][0]
+    washout = int(space['washout'])
+
+    return reservoir_size, w_sparsity, leak_rate, input_scaling, input_sparsity, spectral_radius, feature, aggregation, \
+           state_gram, feedbacks_sparsity, lang, embedding, ridge_param, washout
+# end get_params
+
+
+# Converter in
+def converter_in(converters_desc, converter):
+    """
+    Is the converter in the desc
+    :param converters_desc:
+    :param converter:
+    :return:
+    """
+    for converter_desc in converters_desc:
+        if converter in converter_desc:
+            return True
+        # end if
+    # end for
+    return False
+# end converter_in
 
 
 # Create tokenizer
