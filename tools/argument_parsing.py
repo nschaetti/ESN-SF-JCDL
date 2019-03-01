50fc9bc014248b3f834a56356033791c3f556dc2
nschaetti <n.schaetti@gmail.com>
2019-03-01 12:54:27 +0100

    Argument parsing

--- tools/argument_parsing.py
+++ tools/argument_parsing.py
@@ -28,6 +28,7 @@ def parser_esn_training():
     # Author parameters
     args.add_argument(command="--author", name="author", type=str, help="Author to test", extended=False)
     args.add_argument(command="--novels", name="novels", action='store_true', help="Add novels ?", default=False, extended=False)
+    args.add_argument(command="--remove_authors", name="remove_authors", action='store_true', help="Remove author in text", default=False, extended=False)
 
     # ESN arguments
     args.add_argument(command="--reservoir-size", name="reservoir_size", type=float, help="Reservoir's size",
