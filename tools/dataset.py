eafdd9801464a009535f3e8929c7f02e5bfa2eb6
nschaetti <n.schaetti@gmail.com>
2019-03-01 12:54:42 +0100

    Remove authors from dataset

--- tools/dataset.py
+++ tools/dataset.py
@@ -14,16 +14,26 @@ import settings
 
 
 # Load dataset
-def load_dataset(author, load_type, shuffle=True):
+def load_dataset(author, load_type, shuffle=False, remove_authors=False):
     """
     Load dataset
     :return:
     """
+    # Remove
+    if remove_authors:
+        remove_text = [u"Isaac", u"isaac", u"ISAAC", u"Asimov", u"ASIMOV", u"asimov", u"Dick", u"DICK", u"dick",
+                       u"Philip", u"PHILIP", u"philip", u"Robert", u"ROBERT", u"robert", u"Silverberg",
+                       u"SILVERBERG", u"silververg"]
+    else:
+        remove_text = []
+    # end if
+
     # Load from directory
     sfgram_dataset = torchlanguage.datasets.SFGramDataset(
         author=author,
         download=True,
-        load_type=load_type
+        load_type=load_type,
+        remove_texts=remove_text
     )
 
     # SFGram dataset training
