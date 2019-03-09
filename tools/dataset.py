4e8f0552fa78510cab9c5273cfbd1e1c21d71a3d
nschaetti <n.schaetti@gmail.com>
2019-03-09 20:47:29 +0100

    homogenous

--- tools/dataset.py
+++ tools/dataset.py
@@ -14,7 +14,7 @@ import settings
 
 
 # Load dataset
-def load_dataset(author, load_type, shuffle=False, remove_authors=False):
+def load_dataset(author, load_type, shuffle=False, remove_authors=False, homogenous=True):
     """
     Load dataset
     :return:
@@ -33,21 +33,30 @@ def load_dataset(author, load_type, shuffle=False, remove_authors=False):
         author=author,
         download=True,
         load_type=load_type,
-        remove_texts=remove_text
+        remove_texts=remove_text,
+        homogeneous=homogenous
     )
 
     # SFGram dataset training
     sfgram_loader_train = torch.utils.data.DataLoader(
-        torchlanguage.utils.CrossValidation(sfgram_dataset),
+        torchlanguage.utils.CrossValidationWithDev(sfgram_dataset, k=5, train='train'),
+        batch_size=1,
+        shuffle=shuffle
+    )
+
+    # SFGram dataset dev
+    sfgram_loader_dev = torch.utils.data.DataLoader(
+        torchlanguage.utils.CrossValidationWithDev(sfgram_dataset, k=5, train='dev'),
         batch_size=1,
         shuffle=shuffle
     )
 
     # SFGram dataset test
     sfgram_loader_test = torch.utils.data.DataLoader(
-        torchlanguage.utils.CrossValidation(sfgram_dataset, train=False),
+        torchlanguage.utils.CrossValidationWithDev(sfgram_dataset, k=5, train='test'),
         batch_size=1,
         shuffle=shuffle
     )
-    return sfgram_dataset, sfgram_loader_train, sfgram_loader_test
+
+    return sfgram_dataset, sfgram_loader_train, sfgram_loader_dev, sfgram_loader_test
 # end load_dataset
